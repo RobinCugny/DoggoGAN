197368898aaf9caeda3facd67db873099beafadf
RobinCugny <robin.cugny@gmail.com>
2019-07-19 18:25:07 +0200

    Adding tensorboard implementation + show_results function

--- doggoGAN.py
+++ doggoGAN.py
@@ -10,12 +10,16 @@ import cv2
 import random
 import scipy.misc
 from utils import *
+import matplotlib.pyplot as plt
 
 slim = tf.contrib.slim
+
+LOGDIR = "tmp/doggoGAN/"
 
 HEIGHT, WIDTH, CHANNEL = 64, 64, 3
 BATCH_SIZE = 64
 EPOCH = 500
+processed=135
 version = 'newDoggos'
 newDoggo_path = './' + version
 
@@ -78,6 +82,11 @@ def generator(input, random_dim, is_train, reuse=False):
         conv1 = tf.reshape(flat_conv1, shape=[-1, s4, s4, c4], name='conv1')
         bn1 = tf.contrib.layers.batch_norm(conv1, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn1')
         act1 = tf.nn.relu(bn1, name='act1')
+        
+        tf.summary.histogram("weights", w1)
+        tf.summary.histogram("biases", b1)
+        tf.summary.histogram("activations", act1)
+            
         # 8*8*256
         #Convolution, bias, activation, repeat! 
         conv2 = tf.layers.conv2d_transpose(act1, c8, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
@@ -85,24 +94,31 @@ def generator(input, random_dim, is_train, reuse=False):
                                            name='conv2')
         bn2 = tf.contrib.layers.batch_norm(conv2, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn2')
         act2 = tf.nn.relu(bn2, name='act2')
+        tf.summary.histogram("activations", act2)
+            
         # 16*16*128
         conv3 = tf.layers.conv2d_transpose(act2, c16, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
                                            kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
                                            name='conv3')
         bn3 = tf.contrib.layers.batch_norm(conv3, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn3')
         act3 = tf.nn.relu(bn3, name='act3')
+        tf.summary.histogram("activations", act3)
+
         # 32*32*64
         conv4 = tf.layers.conv2d_transpose(act3, c32, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
                                            kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
                                            name='conv4')
         bn4 = tf.contrib.layers.batch_norm(conv4, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn4')
         act4 = tf.nn.relu(bn4, name='act4')
+        tf.summary.histogram("activations", act4)
+
         # 64*64*32
         conv5 = tf.layers.conv2d_transpose(act4, c64, kernel_size=[5, 5], strides=[2, 2], padding="SAME",
                                            kernel_initializer=tf.truncated_normal_initializer(stddev=0.02),
                                            name='conv5')
         bn5 = tf.contrib.layers.batch_norm(conv5, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn5')
         act5 = tf.nn.relu(bn5, name='act5')
+        tf.summary.histogram("activations", act5)
         
         #64*64*3
         conv6 = tf.layers.conv2d_transpose(act5, output_dim, kernel_size=[5, 5], strides=[1, 1], padding="SAME",
@@ -110,6 +126,8 @@ def generator(input, random_dim, is_train, reuse=False):
                                            name='conv6')
         # bn6 = tf.contrib.layers.batch_norm(conv6, is_training=is_train, epsilon=1e-5, decay = 0.9,  updates_collections=None, scope='bn6')
         act6 = tf.nn.tanh(conv6, name='act6')
+        tf.summary.histogram("activations", act6)
+
         return act6
 
 
@@ -172,13 +190,19 @@ def train():
     
     # wgan
     fake_image = generator(random_input, random_dim, is_train)
+    tf.summary.image('real_image', real_image, 3)
+    tf.summary.image('fake_image', fake_image, 3)
+
     
     real_result = discriminator(real_image, is_train)
     fake_result = discriminator(fake_image, is_train, reuse=True)
     
     d_loss = tf.reduce_mean(fake_result) - tf.reduce_mean(real_result)  # This optimizes the discriminator.
     g_loss = -tf.reduce_mean(fake_result)  # This optimizes the generator.
-            
+    tf.summary.scalar("discriminator_loss", d_loss)
+    tf.summary.scalar("generator_loss", g_loss)
+    
+    summ = tf.summary.merge_all()
 
     t_vars = tf.trainable_variables()
     d_vars = [var for var in t_vars if 'dis' in var.name]
@@ -198,17 +222,31 @@ def train():
     saver = tf.train.Saver()
     sess.run(tf.global_variables_initializer())
     sess.run(tf.local_variables_initializer())
+    
+    writer = tf.summary.FileWriter(LOGDIR + "test")
+    writer.add_graph(sess.graph)
+    
+    
+#    config = tf.contrib.tensorboard.plugins.projector.ProjectorConfig()
+#    embedding_config = config.embeddings.add()
+#    embedding_config.tensor_name = embedding.name
+#    embedding_config.sprite.image_path = SPRITES
+#    embedding_config.metadata_path = LABELS
+#    # Specify the width and height of a single thumbnail.
+#    embedding_config.sprite.single_image_dim.extend([28, 28])
+#    tf.contrib.tensorboard.plugins.projector.visualize_embeddings(writer, config)
+
     # continue training
     save_path = saver.save(sess, "./model/model.ckpt")
     ckpt = tf.train.latest_checkpoint('./model/' + version)
-    saver.restore(sess, save_path)
+    saver.restore(sess, ckpt)
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
     print('total training sample num:%d' % samples_num)
-    print('batch size: %d, batch num per epoch: %d, epoch num: %d' % (batch_size, batch_num, EPOCH))
+    print('batch size: %d, batch num per epoch: %d, epoch num: %d' % (batch_size, batch_num, EPOCH-processed))
     print('start training...')
-    for i in range(EPOCH):
+    for i in range(processed+1,EPOCH):
         print("Running epoch {}/{}...".format(i, EPOCH))
         for j in range(batch_num):
             print("Batch {}/{}".format(j,batch_num), end="\r")
@@ -223,15 +261,15 @@ def train():
                 sess.run(d_clip)
                 
                 # Update the discriminator
-                _, dLoss = sess.run([trainer_d, d_loss],
+                _, dLoss,s = sess.run([trainer_d, d_loss,summ],
                                     feed_dict={random_input: train_noise, real_image: train_image, is_train: True})
-
             # Update the generator
             for k in range(g_iters):
                 # train_noise = np.random.uniform(-1.0, 1.0, size=[batch_size, random_dim]).astype(np.float32)
                 _, gLoss = sess.run([trainer_g, g_loss],
                                     feed_dict={random_input: train_noise, is_train: True})
 
+        writer.add_summary(s, i)
             # print 'train:[%d/%d],d_loss:%f,g_loss:%f' % (i, j, dLoss, gLoss)
             
         # save check point every 5 epoch
@@ -249,32 +287,56 @@ def train():
             # imgtest.astype(np.uint8)
             save_images(imgtest, [8,8] ,newDoggo_path + '/epoch' + str(i) + '.jpg')
             
-            print('train:[%d],d_loss:%f,g_loss:%f' % (i, dLoss, gLoss))
+        print('train:[%d],d_loss:%f,g_loss:%f' % (i, dLoss, gLoss))
     coord.request_stop()
     coord.join(threads)
 
 
-# def test():
-    # random_dim = 100
-    # with tf.variable_scope('input'):
-        # real_image = tf.placeholder(tf.float32, shape = [None, HEIGHT, WIDTH, CHANNEL], name='real_image')
-        # random_input = tf.placeholder(tf.float32, shape=[None, random_dim], name='rand_input')
-        # is_train = tf.placeholder(tf.bool, name='is_train')
-    
-    # # wgan
-    # fake_image = generator(random_input, random_dim, is_train)
-    # real_result = discriminator(real_image, is_train)
-    # fake_result = discriminator(fake_image, is_train, reuse=True)
-    # sess = tf.InteractiveSession()
-    # sess.run(tf.global_variables_initializer())
-    # variables_to_restore = slim.get_variables_to_restore(include=['gen'])
-    # print(variables_to_restore)
-    # saver = tf.train.Saver(variables_to_restore)
-    # ckpt = tf.train.latest_checkpoint('./model/' + version)
-    # saver.restore(sess, ckpt)
+def test():
+    random_dim = 100
+    with tf.variable_scope('input'):
+        real_image = tf.placeholder(tf.float32, shape = [None, HEIGHT, WIDTH, CHANNEL], name='real_image')
+        random_input = tf.placeholder(tf.float32, shape=[None, random_dim], name='rand_input')
+        is_train = tf.placeholder(tf.bool, name='is_train')
+   
+    # wgan
+    fake_image = generator(random_input, random_dim, is_train)
+    real_result = discriminator(real_image, is_train)
+    fake_result = discriminator(fake_image, is_train, reuse=True)
+    sess = tf.InteractiveSession()
+    sess.run(tf.global_variables_initializer())
+    variables_to_restore = slim.get_variables_to_restore(include=['gen'])
+    print(variables_to_restore)
+    saver = tf.train.Saver(variables_to_restore)
+    ckpt = tf.train.latest_checkpoint('./model/' + version)
+    saver.restore(sess, ckpt)
 
+def show_results(N=64):
+    random_dim = 100
+    with tf.variable_scope('input'):
+        real_image = tf.placeholder(tf.float32, shape = [None, HEIGHT, WIDTH, CHANNEL], name='real_image')
+        random_input = tf.placeholder(tf.float32, shape=[None, random_dim], name='rand_input')
+        is_train = tf.placeholder(tf.bool, name='is_train')
+   
+    fake_image = generator(random_input, random_dim, is_train)
+    sess = tf.InteractiveSession()
+    sess.run(tf.global_variables_initializer())
+    variables_to_restore = slim.get_variables_to_restore(include=['gen'])
+    saver = tf.train.Saver(variables_to_restore)
+    ckpt = tf.train.latest_checkpoint('./model/' + version)
+    saver.restore(sess, ckpt)
+    sample_noise = np.random.uniform(-1.0, 1.0, size=[N, random_dim]).astype(np.float32)
+    imgtest = sess.run(fake_image, feed_dict={random_input: sample_noise, is_train: False})
+    image = inverse_transform(imgtest)
+    image = np.squeeze(merge(image, [8,8]))
+    plt.figure()
+    plt.imshow(image)
+    plt.show()
+#    return image
+    
 
 if __name__ == "__main__":
     train()
-    # test()
+#    show_results()
+#    test()
 
